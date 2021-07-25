0b10bf267feea6a3c9cc2c1f96f8b46942f69143
funbunch <shannan_bunch@hotmail.com>
2021-07-24 22:12:25 -0700

    Setup components for next and imports"

---
+++ components/Peg.ts
@@ -0,0 +1,12 @@
+
+
+export default props => {
+  const activeClass = props.isActive ? "peg--active" : "";
+  const colorClass = `peg--${props.color}`;
+  return (
+    <div
+      onClick={() => props.onClick()}
+      className={`peg ${activeClass} ${colorClass}`}
+    />
+  );
+};
